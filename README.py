ff779a9619beaf4e2446d2890d6d2c97402a62c8
Tim Eller <tseller@gmail.com>
2025-09-24 00:44:44 -0700

    Update README

--- README.py
+++ README.py
@@ -7,5 +7,5 @@ https://webpage-mcp-762632998010.us-central1.run.app
 
 From the command line, run:
 ```bash
-uv run uvicorn server:app --port 8001
+uv run fastmcp run server.py --transport http --port 8001
 ```
